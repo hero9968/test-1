6478de236e185514289c70d37c4b2c7417f4b08b
hero <hero9968@yeah.net>
2015-12-22 09:04:46 +0800

    first

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+printf("\n-----------------------------\n");
+printf("\n  tttttttttkktttttttttttttttt  \n");
+printf("\n-----------------------------\n");
+  printf("\n+++++++++++\n");
+  printf("");
+  //----------------------
+     //------------
+  {
+  
+  }
+  {
+  
+  }
+  return 0;
+
+//  at89c51   centOS ubuntu   suse  open suse
+// redhat   fedora   
+
+
+
+}
+
